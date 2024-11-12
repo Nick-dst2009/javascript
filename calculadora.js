c331811a787962f9a47c420c28747ad5e89b1e19
Nick-dst2009 <nicolysturra@gmail.com>
2024-11-12 16:15:15 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,79 @@
+const ask = require('readline-sync')
+
+let sustentaMenu = true
+
+while (sustentaMenu) {
+    console.log(`
+    [1] - calcular
+    [2] - sair
+    `)
+
+    let opcaoEscolhida = Number(ask.question("Digite a opcao desejada:  "))
+
+    switch (opcaoEscolhida) {
+        case 1:
+            console.log(`
+     [1] - adicao
+     [2] - subtracao
+     [3] - multiplicacao
+     [4] - divisao
+     [5] - mod
+        `)
+
+
+
+            let operacaoDesejada = Number(ask.question("Digite a operacao desejada:  "))
+
+            let n1 = Number(ask.question("Digite o primeiro numero   "))
+            let n2 = Number(ask.question("Digite o segundo numero    "))
+            let resultado
+
+            switch (operacaoDesejada) {
+
+
+                case 1:
+
+                    resultado = n1 + n2
+                    console.log(`${n1} + ${n2} = ${resultado}`)
+                    break
+
+                case 2:
+                    resultado = n1 - n2
+                    console.log(`${n1} - ${n2} = ${resultado}`)
+                    break
+
+
+                case 3:
+                    resultado = n1 * n2
+                    console.log(`${n1} * ${n2} = ${resultado}`)
+                    break
+
+                case 4:
+                    resultado = n1 / n2
+                    console.log(`${n1} / ${n2} = ${resultado}`)
+                    break
+
+
+                case 5:
+                    resultado = n1 % n2
+                    console.log(`${n1} % ${n2} = ${resultado}`)
+                    break
+
+
+                default:
+                    console.log("Opção invalida")
+                    break
+            }
+            break
+
+        case 2:
+            console.log("Voce saiu")
+            sustentaMenu = false
+
+            break
+        default:
+            console.log("Opcao invalida")
+            break
+    }
+
+}
